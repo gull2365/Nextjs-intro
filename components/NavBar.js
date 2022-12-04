89cf490caa4e1cb3e5a2f067241963df721ed9ed
gull2365 <jych2365@gmail.com>
2022-12-04 20:32:05 +0900

    feat: CSS module

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,28 @@
-import Link from "next/link"
+import Link from "next/link";
 import { Router, useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export const NavBar = () => {
-    const router = useRouter();
-    console.log(router);
-    return (
-        <nav>
-            <Link href={"/"}>Home</Link>
-            <Link href={"/about"}>about</Link>
-        </nav>
-    )
-}
+  const router = useRouter();
+  return (
+    <nav>
+      <Link
+        className={`${styles.Link} ${
+          router.pathname === "/" ? styles.active : ""
+        }`}
+        href="/"
+      >
+        Home
+      </Link>
+      <Link
+        className={[
+          styles.Link,
+          router.pathname === "/about" ? styles.active : "",
+        ].join("")}
+        href="/about"
+      >
+        about
+      </Link>
+    </nav>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import { useState } from "react";
 import { NavBar } from "../components/NavBar";
 
 export default function Home() {
-    return (
-        <div>
-            <NavBar></NavBar>
-            <h2>HELLO NEXT</h2>
-        </div>
-    );
-};
+  return (
+    <div>
+      <NavBar></NavBar>
+      <h2>HELLO NEXT</h2>
+    </div>
+  );
+}
